44183a48beb8b6249535f4b7e2592acc74421566
Hester <hester.dsouza.08@gmail.com>
2024-10-29 20:19:45 +0530

    homepage slideshow basic structure

--- steel-website/src/pages/homePage/HomePage.jsx
+++ steel-website/src/pages/homePage/HomePage.jsx
@@ -3,7 +3,14 @@ import "./homePage.css"
 const HomePage = () => {
   return (
     <div className="homePage">        
-        <main></main>
+        <section className="slideshow-container">
+          <div className="slide fade"></div>
+          <div className="slide fade"></div>
+          <div className="slide fade"></div>
+          {/* Arrow buttons */}
+          <button className="prev"></button>
+          <button className="next"></button>
+        </section>
     </div>
   )
 }
