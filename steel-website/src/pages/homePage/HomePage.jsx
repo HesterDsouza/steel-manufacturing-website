11196935d5340c88f7000758c319eacfa357f332
Hester <hester.dsouza.08@gmail.com>
2024-10-30 09:43:15 +0530

    Images location change

--- steel-website/src/pages/homePage/HomePage.jsx
+++ steel-website/src/pages/homePage/HomePage.jsx
@@ -1,15 +1,24 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import "./homePage.css"
+import { faSquareCaretLeft } from "@fortawesome/free-solid-svg-icons"
+import { faSquareCaretRight } from "@fortawesome/free-solid-svg-icons"
 
 const HomePage = () => {
   return (
     <div className="homePage">        
         <section className="slideshow-container">
-          <div className="slide fade"></div>
-          <div className="slide fade"></div>
-          <div className="slide fade"></div>
+          <div className="slide fade">
+            <img src="/fabrication.jpg" alt="" />
+          </div>
+          <div className="slide fade">
+            <img src="" alt="" />
+          </div>
+          <div className="slide fade">
+            <img src="" alt="" />
+          </div>
           {/* Arrow buttons */}
-          <button className="prev"></button>
-          <button className="next"></button>
+          <FontAwesomeIcon className="slide-arrow prev" icon={faSquareCaretLeft}/>
+          <FontAwesomeIcon className="slide-arrow next" icon={faSquareCaretRight}/>
         </section>
     </div>
   )
